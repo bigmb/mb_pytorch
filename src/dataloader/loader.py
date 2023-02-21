28fb962131fad8c06b82e5aa56474f5e08672f66
bigmb <malav.b93@gmail.com>
2023-02-21 15:38:10 +0000

    added basic YAML reader

---
+++ src/dataloader/loader.py
@@ -0,0 +1,10 @@
+#dataloader for pytorch2.0
+
+import torchdata
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+def get_dataloader():
+    """
+    """
---
+++ src/utils/yaml_reader.py
@@ -0,0 +1,32 @@
+#file for reading all the data from yaml file and returning the data
+
+import yaml
+import os
+from mb_utils.src.logging import logger
+
+class YamlReader:
+    """
+    Yaml file reader class
+    Input:
+        Yaml: yaml file path
+    Output:
+        data: list of data from yaml file
+    """
+    def __init__(self,Yaml) -> None:
+        if os.path.exists(Yaml):
+            self.yaml = Yaml
+        else:
+            raise FileNotFoundError("Yaml file not found")
+        self._data = None
+    
+    def data(self,logger=None):
+        """
+        read data from yaml file and return a list
+        """
+        if not self._data:
+            with open(self.yaml,'r',encoding='utf-8') as f:
+                self._data = list(yaml.safe_load_all(f))
+        if logger:
+            logger.info("Read data from yaml file: {}".format(self._data))
+        return self._data
+    

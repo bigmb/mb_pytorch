b98eef1cc2bec511609934d3fdc4dfe82952bf41
bigmb <malav.b93@gmail.com>
2023-03-02 01:49:05 +0000

    meta network

--- src/metalearning/prototypical.py
+++ src/metalearning/prototypical.py
@@ -1,3 +1,104 @@
-import torch
 import torch.nn as nn
-import torch.optim as optim
+import torch
+from torch.autograd import Variable
+import torch.nn.functional as F
+import numpy as np
+from ..utils.dist import euclidean_dist
+
+
+def extract_sample(n_way, n_support, n_query, datax, datay):
+    """
+  Picks random sample of size n_support+n_querry, for n_way classes
+  Args:
+      n_way (int): number of classes in a classification task
+      n_support (int): number of labeled examples per class in the support set
+      n_query (int): number of labeled examples per class in the query set
+      datax (np.array): dataset of images
+      datay (np.array): dataset of labels
+  Returns:
+      (dict) of:
+        (torch.Tensor): sample of images. Size (n_way, n_support+n_query, (dim))
+        (int): n_way
+        (int): n_support
+        (int): n_query
+    """
+    K = np.random.choice(np.unique(datay), n_way, replace=False)
+    sample = np.empty([128],dtype=float)
+    for cls in K:
+        ##pass a df with event id , taxcode, after_image_id, embeddings
+        temp_data = datax[datax['new_taxcode']==cls]
+        datax_cls = np.array(temp_data['feat_vec'])
+
+        perm = np.random.permutation(datax_cls)
+        sample_cls = perm[:(n_support+n_query)]
+        for i in range(len(sample_cls)):
+            sample = np.vstack((sample,np.array(sample_cls[i],dtype=float)))
+    sample = torch.from_numpy(sample)
+    return({
+        'images': sample[1:],
+        'n_way': n_way,
+        'n_support': n_support,
+        'n_query': n_query
+        })
+
+
+class Flatten(nn.Module):
+    def __init__(self):
+        super(Flatten, self).__init__()
+
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+class ProtoNet(nn.Module):
+    def __init__(self):
+        """
+        Args:
+            encoder : CNN encoding the images in sample
+            n_way (int): number of classes in a classification task
+            n_support (int): number of labeled examples per class in the support set
+            n_query (int): number of labeled examples per class in the query set
+        """
+        super(ProtoNet, self).__init__()
+
+    def set_forward_loss(self, sample):
+        """
+        Computes loss, accuracy and output for classification task
+        Args:
+            sample (torch.Tensor): shape (n_way, n_support+n_query, (dim)) 
+        Returns:
+            torch.Tensor: shape(2), loss, accuracy and y_hat
+        """
+        sample_images = sample['images'].cuda()
+        n_way = sample['n_way']
+        n_support = sample['n_support']
+        n_query = sample['n_query']
+
+        x_support = sample_images[:, :n_support]
+        x_query = sample_images[:, n_support:]
+
+        #target indices are 0 ... n_way-1
+        target_inds = torch.arange(0, n_way).view(n_way, 1, 1).expand(n_way, n_query, 1).long()
+        target_inds = Variable(target_inds, requires_grad=False)
+        target_inds = target_inds.cuda()
+
+        #Gettig images in V stack
+        z = sample_images
+        z_dim=z.size(-1)
+        z_proto = z[:n_way*n_support].view(n_way, n_support, z_dim).mean(1)
+        z_query = z[n_way*n_support:]
+
+        #compute distances
+        dists = euclidean_dist(z_query, z_proto)
+
+        #compute probabilities
+        log_p_y = F.log_softmax(-dists, dim=1).view(n_way, n_query, -1)
+
+        loss_val = -log_p_y.gather(2, target_inds).squeeze().view(-1).mean()
+        _, y_hat = log_p_y.max(2)
+        acc_val = torch.eq(y_hat, target_inds.squeeze()).float().mean()
+
+        return loss_val, {
+            'loss': loss_val.item(),
+            'acc': acc_val.item(),
+            'y_hat': y_hat}
+                                                 

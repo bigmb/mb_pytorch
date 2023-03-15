fdd9250f029f92acdfc17df31bf1e474da8e969f
bigmb <malav.b93@gmail.com>
2023-03-15 00:55:16 +0000

    setup.py add

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup,find_packages,find_namespace_packages
+from src.version import version
+
+setup(
+    name="mb_pytorch",
+    version=version,
+    description="Pytorch functions functions package",
+    author=["Malav Bateriwala"],
+    packages=find_namespace_packages(include=["src.*"]),
+    #packages=find_packages(),
+    scripts=['scripts/embeddings/emb.py','scripts/extra_utils/dataload_results.py'],
+    install_requires=[
+        "numpy",
+        "mb_pandas",
+        "mb_utils",
+        "torch",
+        "torchvision"],
+    python_requires='>=3.8',)

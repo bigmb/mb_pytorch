44bd1c67f56101240fb24becccc2fae4cd0bb131
bigmb <malav.b93@gmail.com>
2023-04-19 02:19:15 +0100

    new loader

--- mb_pytorch/utils/viewer.py
+++ mb_pytorch/utils/viewer.py
@@ -237,6 +237,7 @@ def gradcam_viewer(gradcam_layer, model, x_grad,gradcam_rgb=False,use_cuda=False
             return cr
         if cr is not None:        
             #cam_img = new_show_cam_on_image(x_grad[0].numpy(),cr,use_rgb=gradcam_rgb)
-            cam_img = show_cam_on_image(x_grad[0].numpy(), cr,use_rgb=gradcam_rgb)
+            x_grad_new = x_grad[0].permute(1,2,0).numpy()
+            cam_img = show_cam_on_image(x_grad_new, cr,use_rgb=gradcam_rgb)
     return cam_img
     

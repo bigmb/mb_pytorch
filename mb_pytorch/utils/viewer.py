417f72ae50ad699ed9d9ff0e77e6a6aa387c872c
bigmb <malav.b93@gmail.com>
2023-04-19 02:12:30 +0100

    viewer edit

--- mb_pytorch/utils/viewer.py
+++ mb_pytorch/utils/viewer.py
@@ -5,7 +5,8 @@ import torch
 import torchvision
 from torchvision.utils import draw_bounding_boxes, draw_segmentation_masks
 import torchvision.transforms.functional as TF
-from pytorch_grad_cam import GradCAM,show_cam_on_image
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.image import show_cam_on_image
 import io
 import PIL
 

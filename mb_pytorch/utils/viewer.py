7565e9e471668fb965a54de73fa84253c4e96ec9
bigmb <malav.b93@gmail.com>
2023-04-24 01:31:27 +0100

    bug fix

--- mb_pytorch/utils/viewer.py
+++ mb_pytorch/utils/viewer.py
@@ -218,9 +218,9 @@ def new_show_cam_on_image(img, mask, use_rgb=True,image_weight: float = 0.5):
         heatmap = cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB)
     heatmap = np.float32(heatmap) / 255
 
-    if np.max(img) > 1:
-        raise Exception(
-            "The input image should np.float32 in the range [0, 1]")
+    # if np.max(img) > 1:
+    #     raise Exception(
+    #         "The input image should np.float32 in the range [0, 1]")
     if image_weight < 0 or image_weight > 1:
         raise Exception(
             f"image_weight should be in the range [0, 1].\

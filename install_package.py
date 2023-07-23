094e4c6e210fc3fec009bb052e773c898275d9ad
bigmb <malav.b93@gmail.com>
2023-07-24 00:31:24 +0100

    Bug fix commit

--- install_package.py
+++ install_package.py
@@ -1,22 +1,55 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-##Run the make_version file to update the version number and then run this file to install the package and upload it to pipy.
 
 import os
 import subprocess
-file = '/home/malav/mb_pytorch'
+import sys
+
+py_version = sys.version
+print(py_version)
+if py_version[:4] == '3.10':
+    py_requires = 'python3.10'
+else:
+    py_requires = 'python3.8'
+print(py_requires)
+
+file = os.getcwd() 
+
 
 #subprocess.run(["cd",file]), check=True, stdout=subprocess.PIPE).stdout
 os.system('cd ' + file)
+
+os.system('./make_version.sh')
+
+print("version file updated")
+print('*'*100)
+
+subprocess.run(["git", "add", "."], check=True, stdout=subprocess.PIPE).stdout
+subprocess.run(["git", "commit", "-am", "Bug fix commit"], check=True, stdout=subprocess.PIPE).stdout
+
+print('git commit done')
+
+subprocess.run(["git", "pull"], check=True, stdout=subprocess.PIPE).stdout
+print('git pull done')
+print('*'*100)
+
+subprocess.run(["git", "push"], check=True, stdout=subprocess.PIPE).stdout
+print('*'*100)
+print('removing dist and build folders')
 
 if os.path.exists(file+'/dist'):
     os.system('sudo rm -rf '+file+'/dist')
     os.system('sudo rm -rf '+file+'/build')
 #subprocess.run(["ls"]),check=True, stdout=subprocess.PIPE).stdout
 os.system("ls")
-subprocess.run(["git", "pull"], check=True, stdout=subprocess.PIPE).stdout
-#os.system('git pull')
-os.system('python3.8 -m setup bdist_wheel')
-os.system('python3.8 -m pip install '+file + '/dist/' +os.listdir(file +'/dist')[-1])
-os.system('python3.8 -m twine upload dist/*')
+
+os.system(py_requires + ' -m setup bdist_wheel')
+
+print('*'*100)
+print('wheel built')
+os.system(py_requires + ' -m pip install '+file + '/dist/' +os.listdir(file +'/dist')[-1])
+
+print('package installed')
+print('*'*100)
+os.system(py_requires + ' -m twine upload dist/*')

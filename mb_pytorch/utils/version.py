b1f9806d23cc6f8e50d9dbe3f25e374d5e5db0f5
Malav <malav.b93@gmail.com>
2024-07-11 03:14:21 +0100

    updated version.py to <<<<<<< HEAD..1

--- mb_pytorch/utils/version.py
+++ mb_pytorch/utils/version.py
@@ -1,9 +1,5 @@
-MAJOR_VERSION = 1
-MINOR_VERSION = 0
-<<<<<<< HEAD
-PATCH_VERSION = 88
-=======
-PATCH_VERSION = 89
->>>>>>> refs/remotes/origin/main
+MAJOR_VERSION = <<<<<<< HEAD
+MINOR_VERSION = 
+PATCH_VERSION = 1
 version = '{}.{}.{}'.format(MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
 __all__  = ['MAJOR_VERSION', 'MINOR_VERSION', 'PATCH_VERSION', 'version']

de937c9c638717c3e9cf8802c3d38168752582bb
Malav <malav.b93@gmail.com>
2024-09-19 03:18:51 +0100

    updated version.py to 1.2.31

--- mb_pytorch/classification/training.py
+++ mb_pytorch/classification/training.py
@@ -4,9 +4,10 @@ import tqdm
 import os
 from mb_utils.src.logging import logger
 import numpy as np
-from ..utils.viewer import gradcam_viewer,create_img_grid,plot_classes_pred
+from ..utils.viewer import gradcam_viewer,plot_classes_pred,plot_to_image
 from mb_pytorch.models.modelloader import ModelLoader
 from mb_pytorch.training.train_params import train_helper
+from mb.plt.utils import dynamic_plt
 
 __all__ = ['classification_train_loop']
 
@@ -32,7 +33,9 @@ def classification_train_loop( k_yaml: dict,scheduler: Optional[object] =None,wr
     data_model = k_yaml.data_dict['model']
     model_data_load = ModelLoader(k_yaml.data_dict['model'])
     model =  model_data_load.get_model()
-    
+    device_type = data_model['device']
+    bbox_threshold = data_model['model_meta_data']['model_bbox_threshold']
+
     if logger:
         logger.info('Model Loaded')
     
@@ -47,10 +50,19 @@ def classification_train_loop( k_yaml: dict,scheduler: Optional[object] =None,wr
         logger.info(f'Loss: {loss_attr}')
         logger.info(f'Optimizer: {optimizer}')
         logger.info(f'Scheduler: {scheduler}')
-    
-    model.to(device)
 
-    for i in tqdm.tqdm(range(data_model['model_epochs'])):
+    if device_type =='cuda':
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        if device == 'cuda':
+            torch.cuda.empty_cache()
+            if logger:
+                logger.info('Device: CUDA')
+                logger.info(torch.cuda.get_device_name(0))
+
+    model.to(device)
+    best_val_loss = float('inf')
+
+    for epoch in tqdm.tqdm(range(data_model['model_epochs']), desc="Epochs"):
         
         ##train loop
         
@@ -59,126 +71,103 @@ def classification_train_loop( k_yaml: dict,scheduler: Optional[object] =None,wr
         
         if logger:
             logger.info('Training Started')
-        for j,(x,y) in enumerate(train_loader):
-            x,y = x.to(device),y.to(device)
+        for batch_idx, data in enumerate(tqdm.tqdm(train_loader, desc="Training", leave=False)):
+            images,bbox,labels = data.values()
+            images = list(image.to(device) for image in images)
+            bbox = list(b.to(device) for b in bbox)
+            bbox = [b.view(-1, 4) if b.dim() == 1 else b for b in bbox]
+            labels = list(label.to(device) for label in labels)  
+            targets = [{'boxes': b,'labels': label} for b,label in zip(bbox, labels)]      
+                    
             optimizer.zero_grad()
-            y_pred = model(x)
-            current_loss = loss_attr(y_pred,y)
-            current_loss.backward()            
+            loss_dict = model(images, targets)  
+            losses = sum(loss for loss in loss_dict.values())
+            
+            losses.backward()
             optimizer.step()
             if scheduler is not None:
-                scheduler.step()    
-            train_loss += current_loss.item()
-            if logger:
-                logger.info(f'Epoch {i+1} - Batch {j+1} - Train Loss: {current_loss.item()}')
+                scheduler.step()
             
-
+            train_loss += losses.item()
+            if logger:
+                logger.info(f'Epoch {epoch+1} - Batch {batch_idx+1} - Train Loss: {losses.item()}')
+        
         avg_train_loss = train_loss / len(train_loader)
         if logger:
-            logger.info(f'Epoch {i+1} - Train Loss: {avg_train_loss}')
+            logger.info(f'Epoch {epoch+1} - Train Loss: {avg_train_loss}')
             logger.info(f"lr = {optimizer.param_groups[0]['lr']}")
-        
+
         model.train(False)
-    
-        if writer is not None:
-            writer.add_graph(model, x)
-            writer.add_scalar('Loss/train', avg_train_loss, global_step=i)
-            for name, param in model.named_parameters():
-                writer.add_histogram(name, param, global_step=i)
-            
-            x = x.to('cpu')
-            y = y.to('cpu')
-            x_grad = x[0,:]
-            x_grad = x_grad.unsqueeze(0)
-            #y_grad = y[0].to('cpu')
-            
-            create_img_grid(x,y,writer,global_step=i)
 
-            ##gradcam       
-            if gradcam is not None:
-                use_cuda=False
-                if device.type != 'cpu':
-                    use_cuda = True
-                for cam_layers in gradcam:
-                    grad_img = gradcam_viewer(cam_layers,model,x_grad,gradcam_rgb=gradcam_rgb,use_cuda=use_cuda)
-                    if grad_img is not None:
-                        grad_img = np.transpose(grad_img,(2,0,1))
-                        writer.add_image(f'Gradcam training/{cam_layers}',grad_img,global_step=i)
-                    if j == 0:
-                        if grad_img is None:
-                            if logger:
-                                logger.info(f'Gradcam not supported for {cam_layers}')            
-                        
-        #validation loop
-
+        ## Validation loop
         val_loss = 0
-        val_acc = 0
-        new_val_loss = 0
-    
+        val_bbox = []
+        val_labels = []
+        val_scores = []
+        val_targets_labels = []
+        val_targets_bbox = []
+
         with torch.no_grad():
-            for l,(x_val, y_val) in enumerate(val_loader):
-                x_val, y_val = x_val.to(device), y_val.to(device)
-                output = model(x_val)
-                val_loss += loss_attr(output, y_val).item() * x_val.size(0)
-                _, preds = torch.max(output, 1) #no need of softmax. max returns the index of the max value
-                val_acc += torch.sum(preds == y_val.data)
-                new_val_loss = val_loss/x_val.size(0)
+            for batch_idx, data in enumerate(tqdm.tqdm(val_loader, desc="Validation", leave=False)):
+                images,bbox,labels = data.values()
+                images = list(image.to(device) for image in images)
+                bbox = list(b.to(device) for b in bbox)
+                bbox = [b.view(-1, 4) if b.dim() == 1 else b for b in bbox]
+                labels = list(label.to(device) for label in labels)  
+                targets = [{'boxes': b,'labels': label} for b,label in zip(bbox, labels)]    
+
+                loss_dict = model(images, targets)  ## output is boxes, labels, scores
+
+                if len(loss_dict) == 0:
+                    continue
+                else:
+                    for i in range(len(loss_dict)):
+                        if len(loss_dict[i]['boxes']) > 0 :
+                            if 'scores' in loss_dict[i]:
+                                for j in range(len(loss_dict[i]['scores'])):
+                                    if loss_dict[i]['scores'][j] > bbox_threshold:
+                                        val_bbox.append(loss_dict[i]['boxes'][j])
+                                        val_labels.append(loss_dict[i]['labels'][j])
+                                        val_scores.append(loss_dict[i]['scores'][j])
+                                        if j < 1:
+                                            val_targets_labels.append(labels[i])
+                                            val_targets_bbox.append(bbox[i])
+
+                #losses = sum(loss for loss in loss_dict.values() if loss.numel()>0)
+                
+                val_loss += losses.item() * len(images)
                 if logger: 
-                    logger.info(f'Epoch {i+1} - Batch {l+1} - Val Loss: {new_val_loss:.3f}')
+                    logger.info(f'Epoch {epoch+1} - Batch {batch_idx+1} - Val Loss: {losses.item()}')
             
             avg_val_loss = val_loss / len(val_loader.dataset)
-            val_acc = val_acc/len(val_loader.dataset)
-
+            
             if logger:
-                logger.info(f'Epoch {i+1} -Avg Val Loss: {avg_val_loss:.3f}')
-                logger.info(f'Epoch {i+1} - Val Accuracy: {val_acc:.3f}')
-    
-    
+                logger.info(f'Epoch {epoch+1} - Avg Val Loss: {avg_val_loss:.3f}')
+
+        # TensorBoard logging
         if writer is not None:
-            writer.add_scalar('Loss/val', val_loss, global_step=i)
-            writer.add_scalar('Accuracy/val', val_acc, global_step=i)    
+            writer.add_scalar('Loss/train', avg_train_loss, global_step=epoch)
+            writer.add_scalar('Loss/val', avg_val_loss, global_step=epoch)
+            writer.add_scalar('Learning_rate', optimizer.param_groups[0]['lr'], global_step=epoch)
             
-            x_val = x_val.to('cpu')
-            y_val = y_val.to('cpu')
-            x_grad = x_val[0,:]
-            x_grad = x_grad.unsqueeze(0)
-            #y_grad = y_val[0].to('cpu')
+            for name, param in model.named_parameters():
+                writer.add_histogram(name, param, global_step=epoch)
             
-            #get classes/labels in a dict for the last batch
-            if len(x_val)<4:
-                logger.info('Batch size of last batch is less than 4. Cannot plot classes')
-            else:
-                prob_val = torch.nn.functional.softmax(output, dim=1)
-                prob_val = prob_val.to('cpu')
-                preds = preds.to('cpu')
-                fig1 = plot_classes_pred(x_val, y_val, prob_val, preds)
-                writer.add_figure('predictions vs. actuals', fig1, global_step=i)
-            
-            ##gradcam       
-            if gradcam is not None:
-                use_cuda=False
-                if device.type != 'cpu':
-                    use_cuda = True
-                for cam_layers in gradcam:
-                    grad_img = gradcam_viewer(cam_layers,model,x_grad,gradcam_rgb=gradcam_rgb,use_cuda=use_cuda)
-                    if grad_img is not None:
-                        grad_img = np.transpose(grad_img,(2,0,1))
-                        writer.add_image(f'Gradcam/{cam_layers}',grad_img,global_step=i)
-                    if j == 0:
-                        if grad_img is None:
-                            if logger:
-                                logger.info(f'Gradcam not supported for {cam_layers}')   
-    
-        # save best model
-        if i == 0:
-            best_val_loss = float('inf')
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
+            # Visualizations
+            if len(images) > 0:
+                img_l = [np.array(i.to('cpu')) for i in images]
+                labels_l = [str(list(np.array((l.to('cpu'))))[0]) for l in labels] ##check for multiple labels
+                #create_img_grid(x, x, writer, global_step=epoch)
+                fig = dynamic_plt(img_l,labels=labels_l,bboxes=val_targets_bbox,return_fig=True)
+                writer.add_image('grid', plot_to_image(fig), global_step=epoch)
+   
+        # Save best model
+        if avg_val_loss < best_val_loss:
+            best_val_loss = avg_val_loss
             best_model = model.state_dict()
 
-            path = os.path.join(k_data['work_dir'], 'best_model.pth')
+            path = os.path.join(os.path.dirname(k_data['file']['root']), 'best_model.pth')
             torch.save(best_model, path)
             if logger:
-                logger.info(f'Epoch {i+1} - Best Model Saved')
-    
+                logger.info(f'Epoch {epoch + 1} - Best Model Saved (Val Loss: {best_val_loss:.4f})')
         
